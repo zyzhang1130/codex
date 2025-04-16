13c776411a8a17376b841288eadc48328e8f5473
Fouad Matin <169186268+fouad-openai@users.noreply.github.com>
2025-04-16 15:49:32 -0700

    bump(version): 0.1.2504161510 (#135)
    Signed-off-by: Fouad Matin <fouad@openai.com>

--- codex-cli/src/utils/session.ts
+++ codex-cli/src/utils/session.ts
@@ -1,4 +1,4 @@
-export const CLI_VERSION = "0.1.04161352"; // Must be in sync with package.json.
+export const CLI_VERSION = "0.1.2504161510"; // Must be in sync with package.json.
 export const ORIGIN = "codex_cli_ts";
 
 export type TerminalChatSession = {
