107d2ce4e74618968b2eb7016777121d9529a204
Michael Bolin <mbolin@openai.com>
2025-08-07 10:13:13 -0700

    fix: change OPENAI_DEFAULT_MODEL to "gpt-5" (#1943)

--- codex-rs/core/src/config.rs
+++ codex-rs/core/src/config.rs
@@ -9,7 +9,6 @@ use crate::config_types::ShellEnvironmentPolicy;
 use crate::config_types::ShellEnvironmentPolicyToml;
 use crate::config_types::Tui;
 use crate::config_types::UriBasedFileOpener;
-use crate::flags::OPENAI_DEFAULT_MODEL;
 use crate::model_family::ModelFamily;
 use crate::model_family::find_family_for_model;
 use crate::model_provider_info::ModelProviderInfo;
@@ -25,6 +24,8 @@ use std::path::PathBuf;
 use tempfile::NamedTempFile;
 use toml::Value as TomlValue;
 use toml_edit::DocumentMut;
+
+const OPENAI_DEFAULT_MODEL: &str = "gpt-5";
 
 /// Maximum number of bytes of the documentation that will be embedded. Larger
 /// files are *silently truncated* to this size so we do not take up too much of
