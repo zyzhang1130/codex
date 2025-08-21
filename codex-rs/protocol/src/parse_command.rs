74683bab91e46ece256558e51240ccb241c51240
Gabriel Peal <gpeal@users.noreply.github.com>
2025-08-20 22:34:46 -0700

    Add a serde tag to ParsedItem (#2546)

--- codex-rs/protocol/src/parse_command.rs
+++ codex-rs/protocol/src/parse_command.rs
@@ -2,6 +2,7 @@ use serde::Deserialize;
 use serde::Serialize;
 
 #[derive(Debug, Clone, PartialEq, Eq, Deserialize, Serialize)]
+#[serde(tag = "type", rename_all = "snake_case")]
 pub enum ParsedCommand {
     Read {
         cmd: String,
